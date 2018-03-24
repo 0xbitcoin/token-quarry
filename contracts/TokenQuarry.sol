a56bc7c6d942352d81ab8a0bdee62291227c98b5
0xbitcoin <36060731+0xbitcoin@users.noreply.github.com>
2018-03-23 21:14:57 -0400

    Update TokenQuarry.sol

--- contracts/TokenQuarry.sol
+++ contracts/TokenQuarry.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.4.18;
 /*
 
 An ERC20 token wallet which dispenses tokens via Proof of Work mining.
+Based on recommendation from /u/diego_91
 
 Anyone can deposit any ERC20 token in this contract and they will be locked inside.   0xBitcoin miners can submit their solutions to this contract when they submit their solutions to the 0xBitcoin contract and they will then be rewarded with 5% of the ERC20 tokens of their choice which are stored in this contract. 
 
